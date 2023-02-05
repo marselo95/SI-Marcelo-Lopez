f8d26b4d715df8040f8934cf85e3217ce590be5a
marselo95 <97412737+marselo95@users.noreply.github.com>
2023-02-05 11:03:46 -0400

    final creo

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const skillContainer = document.getElementById('skills');
 const slider = document.getElementById('slider');
-const btnL = document.getElementById('btn-l')
-const btnR = document.getElementById('btn-r')
+const btnL = document.getElementById('btn-l');
+const btnR = document.getElementById('btn-r');
+const form = document.getElementById('formulario');
+const letras = ['á', 'é', 'í', 'ó', 'ú', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 let sliderSection = document.querySelectorAll(".slider-section");
 let sliderSectionLast = sliderSection[sliderSection.length - 1];
@@ -12,23 +14,99 @@ document.addEventListener('DOMContentLoaded', function () {
     startApp();
 });
 
-function startApp(){
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let nombre = validateName();
+    let email = ValidateEmail();
+    const nam = document.getElementById('nombre');
+    const crr = document.getElementById('email');
+    const mensaje = document.getElementById('mensaje');
+
+    if (nombre === false) {
+        form.innerHTML += `<p class="warning red wn">Nombre invalido</P>`
+
+        setTimeout(function () {
+            const alert = document.querySelector('.wn');
+            alert.remove()
+        }, 5000)
+
+    }
+    if (email === false) {
+        form.innerHTML += `<p class="warning red en">email invalido</P>`
+
+        setTimeout(function () {
+            const alert = document.querySelector('.en');
+            alert.remove()
+        }, 5000)
+
+    }
+
+    if (email === true && nombre === true) {
+        alert(nam.value + "\n" + crr.value + "\n" + mensaje.value);
+        form.innerHTML += `<p class="warning green s">Mensaje enviado exitosamente</P>` 
+
+        setTimeout(function () {
+            const alert = document.querySelector('.s');
+            alert.remove()
+        }, 5000)
+
+    }
+
+
+})
+
+function ValidateEmail() {
+    const email = document.getElementById('email');
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value) && email.value.length != 0) {
+        return (true)
+    } else {
+        alert("You have entered an invalid email address!")
+        return false
+    }
+
+}
+
+
+function validateName() {
+
+    const name = document.getElementById('nombre');
+
+    if (name.value.length === 0) {
+        return false
+    }
+
+    for (let index = 0; index < name.value.length; index++) {
+        // console.log(name.value[index].toLowerCase())
+        if (letras.includes(name.value[index].toLowerCase())) {
+            continue
+        } else {
+            alert("El nombre no puede contener caracteres especiales");
+            return false;
+        }
+
+    }
+
+    return true
+}
+
+
+function startApp() {
     scrollNav();
 }
 
-function start(){
+function start() {
     scrollNav();
 }
 
 function right() {
     let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
     slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.4s";
+    slider.style.transition = "all 0.5s";
     setTimeout(function () {
         slider.style.transition = "none";
         slider.insertAdjacentElement('beforeend', sliderSectionFirst);
         slider.style.marginLeft = "-100%";
-    }, 400);
+    }, 500);
 }
 
 function left() {
@@ -36,12 +114,12 @@ function left() {
     let sliderSection = document.querySelectorAll(".slider-section");
     let sliderSectionLast = sliderSection[sliderSection.length - 1];
     slider.style.marginLeft = "0";
-    slider.style.transition = "all 0.4s";
+    slider.style.transition = "all 0.5s";
     setTimeout(function () {
         slider.style.transition = "none";
         slider.insertAdjacentElement('afterbegin', sliderSectionLast);
         slider.style.marginLeft = "-100%";
-    }, 400);
+    }, 500);
 }
 
 btnR.addEventListener('click', function () {
@@ -51,6 +129,10 @@ btnR.addEventListener('click', function () {
 btnL.addEventListener('click', function () {
     left();
 })
+
+setInterval(function () {
+    right();
+}, 5000)
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -118,7 +200,6 @@ function eventListener() {
 }
 
 function navResponsive() {
-    console.log('click')
     const nav = document.querySelector('.barra')
     nav.classList.toggle('show')
 }
@@ -126,11 +207,11 @@ function navResponsive() {
 function scrollNav() {
     const links = document.querySelectorAll('.navbar a');
     links.forEach(link => {
-        link.addEventListener('click', function(e) {
+        link.addEventListener('click', function (e) {
             e.preventDefault();
             const scrollSection = e.target.attributes.href.value;
             const section = document.querySelector(scrollSection);
-            section.scrollIntoView({behavior:"smooth"});
+            section.scrollIntoView({ behavior: "smooth" });
         });
     });
 }
@@ -144,5 +225,3 @@ var skills = {
     "Python": 75,
     "Procrastinar": 100
 }
-
-
