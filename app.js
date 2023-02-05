f0fb9453c521caa65a275d408b16d2c2c3432e6d
marselo95 <97412737+marselo95@users.noreply.github.com>
2023-02-05 11:05:40 -0400

    arreglitos

--- app.js
+++ app.js
@@ -60,7 +60,6 @@ function ValidateEmail() {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value) && email.value.length != 0) {
         return (true)
     } else {
-        alert("You have entered an invalid email address!")
         return false
     }
 
